3be9e8ba0ea6f00df773dff18b40aac68207185e
RajeshNandhan <rajeshn7k@gmail.com>
2023-08-19 17:08:17 +0530

    Command Design Pattern

---
+++ BehavioralPatterns/Command/2/OnlineShoppingInvoker2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace BehavioralPatterns.Command
+{
+    internal class OnlineShoppingInvoker2
+    {
+        private List<ICommand> commandsForExecute;
+        private List<ICommand> commandsForUnExecute;
+
+        public void ShoppingCartAndProductInventoryExecuteCommandsToQue(ICommand addProductInventoryCommand, ICommand addShoppingCartCommand)
+        {
+            if (commandsForExecute == null)
+            {
+                commandsForExecute = new List<ICommand>();
+            }
+
+            commandsForExecute.Add(addShoppingCartCommand);
+            commandsForExecute.Add(addProductInventoryCommand);
+        }
+
+        public void ShoppingCartAndProductInventoryUnExecuteCommandsToQue(ICommand removeProductInventoryCommand, ICommand removeShoppingCartCommand)
+        {
+            if (commandsForUnExecute == null)
+            {
+                commandsForUnExecute = new List<ICommand>();
+            }
+
+            commandsForUnExecute.Add(removeShoppingCartCommand);
+            commandsForUnExecute.Add(removeProductInventoryCommand);
+        }
+
+        public void PlaceOrderCommandInvoker()
+        {
+            foreach (var command in commandsForExecute)
+            {
+                command.Execute();
+            }
+
+            foreach (var command in commandsForUnExecute)
+            {
+                command.UnExecute();
+            }
+        }
+    }
+}
--- BehavioralPatterns/Command/CommandClient.cs
+++ BehavioralPatterns/Command/CommandClient.cs
@@ -4,18 +4,14 @@ namespace BehavioralPatterns.Command
 {
     internal class CommandClient
     {
-        public static void BuyProduct()
+        public static void BuyProduct_OnlineShoppingInvoker()
         {
             bool canContinue;
             string consoleKeyInfo;
+
             //Section 1
             IShoppingCartRepository shoppingCartRepository = new ShoppingCartRepository();
             IProductInventoryRepository productInventoryRepository = new ProductInventoryRepository();
-
-            //Section 2
-            ICommand productInventoryCommand = null;
-            ICommand shoppingCartCommand = null;
-            OnlineShoppingInvoker onlineShoppingInvoker = null;
 
             do
             {
@@ -27,7 +23,6 @@ namespace BehavioralPatterns.Command
 
                 string userSelectedProduct = string.Empty;
                 bool canAddItem = false;
-                int userSelectedQuantity = 0;
                 canContinue = true;
 
                 switch (userSelectedProductKey)
@@ -66,18 +61,18 @@ namespace BehavioralPatterns.Command
                     Console.WriteLine("Please enter Number of Items.");
                     consoleKeyInfo = Console.ReadLine();
                     string numberofItemsKeyed = consoleKeyInfo.ToString().ToLower();
-                    userSelectedQuantity = int.Parse(numberofItemsKeyed);
+                    int userSelectedQuantity = int.Parse(numberofItemsKeyed);
 
-                    //Section 3
                     if (userSelectedQuantity > 0)
                     {
-                        productInventoryCommand = new ProductInventoryCommand(
+                        //Section 2
+                        ICommand productInventoryCommand = new ProductInventoryCommand(
                           productInventoryRepository, userSelectedProduct, userSelectedQuantity);
 
-                        shoppingCartCommand = new ShoppingCartCommand(
+                        ICommand shoppingCartCommand = new ShoppingCartCommand(
                             shoppingCartRepository, userSelectedProduct, userSelectedQuantity);
 
-                        onlineShoppingInvoker = new OnlineShoppingInvoker(productInventoryCommand, shoppingCartCommand);
+                        OnlineShoppingInvoker onlineShoppingInvoker = new OnlineShoppingInvoker(productInventoryCommand, shoppingCartCommand);
 
                         if (canAddItem)
                         {
@@ -88,16 +83,109 @@ namespace BehavioralPatterns.Command
                     }
                 }
 
-                //Section 4
+                //Section 3
                 Console.WriteLine(shoppingCartRepository.GetItemsInShoppingCartToString());
                 Console.WriteLine(productInventoryRepository.GetProductInventoryItemsAvailablesToString());
 
             } while (canContinue);
         }
 
+        public static void BuyProduct_OnlineShoppingInvoker2()
+        {
+            bool canContinue;
+            string consoleKeyInfo;
+            //Section 1
+            IShoppingCartRepository shoppingCartRepository = new ShoppingCartRepository();
+            IProductInventoryRepository productInventoryRepository = new ProductInventoryRepository();
+
+            //Section 2
+            OnlineShoppingInvoker2 onlineShoppingInvoker = new OnlineShoppingInvoker2(); ;
+
+            do
+            {
+                Console.WriteLine("Add Item (I - IPod, L - Laptop, M - Mobile),  Remove Item (II - IPod,  LL - Laptop,  MM - Mobile)");
+                Console.WriteLine("Press Any other key to exit and ORDER PLACEMENT.");
+
+                consoleKeyInfo = Console.ReadLine();
+                string userSelectedProductKey = consoleKeyInfo.ToString().ToLower();
+
+                string userSelectedProduct = string.Empty;
+                bool canAddItem = false;
+                canContinue = true;
+
+                switch (userSelectedProductKey)
+                {
+                    case "l":
+                        userSelectedProduct = "Laptop";
+                        canAddItem = true;
+                        break;
+                    case "m":
+                        userSelectedProduct = "Mobile";
+                        canAddItem = true;
+                        break;
+                    case "i":
+                        userSelectedProduct = "IPod";
+                        canAddItem = true;
+                        break;
+                    case "ll":
+                        userSelectedProduct = "Laptop";
+                        canAddItem = false;
+                        break;
+                    case "mm":
+                        userSelectedProduct = "Mobile";
+                        canAddItem = false;
+                        break;
+                    case "ii":
+                        userSelectedProduct = "IPod";
+                        canAddItem = false;
+                        break;
+                    default:
+                        canContinue = false;
+                        break;
+                }
+
+                if (canContinue)
+                {
+                    Console.WriteLine("Please enter Number of Items.");
+                    consoleKeyInfo = Console.ReadLine();
+                    string numberofItemsKeyed = consoleKeyInfo.ToString().ToLower();
+                    int userSelectedQuantity = int.Parse(numberofItemsKeyed);
+
+                    //Section 3
+                    if (userSelectedQuantity > 0)
+                    {
+                        ICommand productInventoryCommand = new ProductInventoryCommand(
+                          productInventoryRepository, userSelectedProduct, userSelectedQuantity);
+
+                        ICommand shoppingCartCommand = new ShoppingCartCommand(
+                            shoppingCartRepository, userSelectedProduct, userSelectedQuantity);
+
+                        if (canAddItem)
+                        {
+                            onlineShoppingInvoker.ShoppingCartAndProductInventoryExecuteCommandsToQue(productInventoryCommand, shoppingCartCommand);
+                        }
+                        else
+                            onlineShoppingInvoker.ShoppingCartAndProductInventoryUnExecuteCommandsToQue(productInventoryCommand, shoppingCartCommand);
+                    }
+                }
+            } while (canContinue);
+
+            //Section 4
+            onlineShoppingInvoker.PlaceOrderCommandInvoker();
+
+            Console.WriteLine(shoppingCartRepository.GetItemsInShoppingCartToString());
+            Console.WriteLine(productInventoryRepository.GetProductInventoryItemsAvailablesToString());
+
+
+            Console.WriteLine("Thank you, Press any key again for exit!!!");
+
+            Console.ReadKey();
+        }
+
         public static void TestMethod()
         {
-            BuyProduct();
+            //BuyProduct_OnlineShoppingInvoker();
+            BuyProduct_OnlineShoppingInvoker2();
         }
     }
 }
